780efe9fbbfafc1642be0c5ecc2510ca0dabae83
apurbagoon1 <apurbagoon1@gmail.com>
2025-03-09 01:27:53 +0600

    added Task Left and Task Complete

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 // Random Background Color
 const themeChange = document.getElementById('theme');
 
-themeChange.addEventListener('click', function () {
+themeChange.addEventListener('click', function() {
     document.documentElement.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
 });
 
@@ -13,6 +13,29 @@ liveDay.innerText = liveDayDate.toLocaleDateString("en-US", {weekday: "short"}) 
 
 const liveDate = document.getElementById("date");
 liveDate.innerText = liveDayDate.toLocaleDateString("en-US", {month: "short", day: "numeric", year: "numeric"});
+
+// Task Left and Task Complete
+const allButtons = document.getElementsByClassName('complete');
+const leftTasks = document.getElementById('task-left');
+const completeTasks = document.getElementById('task-completed');
+
+for (let i = 0; i < allButtons.length; i++) {
+    allButtons[i].addEventListener('click', function () {
+        let leftTasksCount = parseInt(leftTasks.innerText);
+        let completeTasksCount = parseInt(completeTasks.innerText);
+
+        if (leftTasksCount > 0) {
+            leftTasks.innerText = leftTasksCount - 1;
+            completeTasks.innerText = completeTasksCount + 1;
+        }
+
+        allButtons[i].disabled = true;
+        allButtons[i].classList.add('opacity-30');
+        allButtons[i].classList.remove('cursor-pointer');
+    });
+}
+
+
 
 
 
