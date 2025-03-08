a0a616eea3f8c2c065b1218a09ef72efd914fb1a
apurbagoon1 <apurbagoon1@gmail.com>
2025-03-09 00:26:11 +0600

    added Random Background Color

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,19 @@
+// Random Background Color
+const themeChange = document.getElementById('theme');
+
+themeChange.addEventListener('click', function () {
+    document.documentElement.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+});
+
 // Live Day and Date
-const liveDate = new Date();
+const liveDayDate = new Date();
 
-document.getElementById("day").innerText = liveDate.toLocaleDateString("en-US", {weekday: "short"}) + " ,";
+const liveDay = document.getElementById("day");
+liveDay.innerText = liveDayDate.toLocaleDateString("en-US", {weekday: "short"}) + " ,";
 
-document.getElementById("date").innerText = liveDate.toLocaleDateString("en-US", {month: "short", day: "numeric", year: "numeric"});
+const liveDate = document.getElementById("date");
+liveDate.innerText = liveDayDate.toLocaleDateString("en-US", {month: "short", day: "numeric", year: "numeric"});
+
+
+
+
